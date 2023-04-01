e1fb654887c73892bbde1d54641906c539d6ac57
neetu-749 <chaudharyneetu749@gmail.com>
2023-04-01 20:32:43 +0530

    form

---
+++ backend/router/videoList.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+const VideoDetails = require('../models/VideoDetailsSchema');
+
+router.get('/', (req, res, next) => {
+  // console.log("yess");
+  VideoDetails
+    .find()
+    .exec()
+    .then(docs => {
+      console.log(docs);
+      res.status(200).json(docs);
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
+
+module.exports = router;
